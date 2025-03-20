13dd34f187ff061c17816e68bee5b5a1ff5ab1f2
esinnuralasirt <esinnuralasirt32@gmail.com>
2025-03-20 17:04:47 +0200

    qertyu

--- CineGorilla/AppDelegate.swift
+++ CineGorilla/AppDelegate.swift
@@ -1,49 +1,225 @@
-import UIKit
-import FirebaseAuth
+
+import SwiftUI
+import AppTrackingTransparency
+import AdSupport
 import FirebaseCore
+import FirebaseAnalytics
+import FirebaseInstallations
+import FirebaseRemoteConfigInternal
+import SdkPushExpress
+import AppsFlyerLib
 import CoreData
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, DeepLinkDelegate {
+    
     var window: UIWindow?
+    weak var initVC: ViewController?
+    
+    var identAdvert: String = ""
+    var time = 0
+    var analytId: String = ""
 
+    static var orientation = UIInterfaceOrientationMask.all
+    
+    private let pushAppId = "38232-1202"
+    private var externalId = ""
+    private var remote: RemoteConfig?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         FirebaseApp.configure()
+        remote = RemoteConfig.remoteConfig()
+        setupConfig()
         
-        if #available(iOS 13, *) {
-        } else {
-            window = UIWindow(frame: UIScreen.main.bounds)
-            
-            if Auth.auth().currentUser != nil {
-                window?.rootViewController = MainController()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = ViewController()
+        initVC = viewController
+        window?.rootViewController = initVC
+        window?.makeKeyAndVisible()
+        AppsFlyerLib.shared().appsFlyerDevKey = "YHPbRGHTtXw9P3Qja88ugg"
+        AppsFlyerLib.shared().appleAppID = "6743405380"
+        AppsFlyerLib.shared().deepLinkDelegate = self
+        AppsFlyerLib.shared().delegate = self
+        
+        Task { @MainActor in
+            analytId = await fetchAnalyticsId()
+            externalId = analytId
+        }
+
+        loadApp(viewController: viewController)
+        
+        AppsFlyerLib.shared().start()
+        AppsFlyerLib.shared().waitForATTUserAuthorization(timeoutInterval: 60)
+
+        //MARK: - PUSH EXPRESS
+        externalId = analytId
+
+        let options: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(options: options) { granted, error in
+            if error != nil {
             } else {
-                window?.rootViewController = WelcomeController()
+                DispatchQueue.main.async {
+                    application.registerForRemoteNotifications()
+                }
             }
-            
-            window?.makeKeyAndVisible()
         }
         
+        UNUserNotificationCenter.current().delegate = self
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            do {
+                try PushExpressManager.shared.initialize(appId: self.pushAppId)
+                try PushExpressManager.shared.activate(extId: self.externalId)
+            } catch {
+                print("Error initializing or activating PushExpressManager: \(error)")
+            }
+
+            if !PushExpressManager.shared.notificationsPermissionGranted {
+                print("Notifications permission not granted. Please enable notifications in Settings.")
+            }
+        }
+       
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    func fetchAnalyticsId() async -> String {
+        do {
+            if let appInstanceID = Analytics.appInstanceID() {
+                return appInstanceID
+            } else {
+                return ""
+            }
+        }
+    }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientation
+    }
+    
+    func setupConfig() {
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
+        remote?.configSettings = settings
+    }
+    
+    func loadApp(viewController: ViewController) {
+        remote?.fetch { [weak self] status, error in
+            guard let self = self else { return }
+            
+            if status == .success {
+                let appsID = AppsFlyerLib.shared().getAppsFlyerUID()
+                
+                self.remote?.activate { _, error in
+                    DispatchQueue.main.async {
+                        if error != nil {
+                            viewController.firstOpen()
+                            return
+                        }
+                        
+                        if let remString = self.remote?.configValue(forKey: "gorilla").stringValue {
+                            if !remString.isEmpty {
+                                if let finalURL = UserDefaults.standard.string(forKey: "finalURL") {
+                                    viewController.secondOpen(string: finalURL)
+                                    print("SECOND OPEN: \(finalURL)")
+                                    return
+                                }
+                                
+                                if self.identAdvert.isEmpty {
+                                    self.time = 5
+                                    self.identAdvert = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+                                }
+                                
+                                if self.identAdvert.isEmpty {
+                                    viewController.firstOpen()
+                                    return
+                                }
+                                
+                                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(self.time)) {
+                                    let stringURL = viewController.setupstr(mstring: remString, deviid: self.analytId, advid: self.identAdvert, appsflif: appsID)
+                                    
+                                    print("Result: \(stringURL)")
+                                    
+                                    guard let url = URL(string: stringURL) else {
+                                        viewController.firstOpen()
+                                        return
+                                    }
+                                    
+                                    if UIApplication.shared.canOpenURL(url) {
+                                        viewController.secondOpen(string: stringURL)
+                                    } else {
+                                        viewController.firstOpen()
+                                    }
+                                }
+                                
+                            } else {
+                                viewController.firstOpen()
+                            }
+                        } else {
+                            viewController.firstOpen()
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        AppsFlyerLib.shared().handleOpen(url, sourceApplication: sourceApplication, withAnnotation: annotation)
+        return true
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        AppsFlyerLib.shared().waitForATTUserAuthorization(timeoutInterval: 60)
+        ATTrackingManager.requestTrackingAuthorization { (status) in
+            self.time = 10
+            switch status {
+            case .authorized:
+                self.identAdvert = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+                self.time = 1
+            case .denied:
+                self.identAdvert = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+            case .notDetermined:
+                print("Not Determined")
+            case .restricted:
+                print("Restricted")
+            @unknown default:
+                print("Unknown")
+            }
+        }
+        AppsFlyerLib.shared().start()
+    }
+    
+    //MARK: - Push Notification Handling
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokPart = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let tok = tokPart.joined()
+        PushExpressManager.shared.transportToken = tok
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register for remote notifications: \(error)")
     }
 
-    // MARK: - Core Data stack
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        let userInfo = notification.request.content.userInfo
+        print("Received notification while app is in foreground: \(userInfo)")
+        completionHandler([.banner, .list, .sound])
+    }
 
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                    didReceive response: UNNotificationResponse,
+                    withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
+        let userInfo = response.notification.request.content.userInfo
+        print("Handling notification response: \(userInfo)")
+        NotificationCenter.default.post(name: Notification.Name("didReceiveRemoteNotification"), object: nil, userInfo: userInfo)
+        completionHandler()
+    }
+    
     lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
@@ -70,9 +246,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         })
         return container
     }()
-
-    // MARK: - Core Data Saving support
-
+    
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
@@ -86,6 +260,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
 }
 
+extension AppDelegate: AppsFlyerLibDelegate {
+    func onConversionDataSuccess(_ data: [AnyHashable: Any]) {
+        print("onConversionDataSuccess \(data)")
+    }
+    
+    func onConversionDataFail(_ error: Error) {
+        
+    }
+}
---
+++ CineGorilla/ViewController.swift
@@ -0,0 +1,69 @@
+
+import UIKit
+import SwiftUI
+import FirebaseAuth
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let cl = UIColor(named: "cl") ?? .white
+        view.backgroundColor = cl
+
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.color = UIColor(red: 73/255, green: 238/255, blue: 112/255, alpha: 1)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.startAnimating()
+
+        view.addSubview(activityIndicator)
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    func firstOpen() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            let onboardingScreen = Auth.auth().currentUser != nil ? MainController() : WelcomeController()
+//            let hostingController = UIHostingController(rootView: onboardingScreen)
+//            self.rootViewC(onboardingScreen)
+            
+            UIApplication.setRootViewController(onboardingScreen)
+        }
+    }
+    
+    func secondOpen(string: String) {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            for child in self.children {
+                if child is FirstVC {
+                    return
+                }
+            }
+            guard !string.isEmpty else { return }
+            let secondController = FirstVC(url: string)
+            self.addChild(secondController)
+            secondController.view.frame = self.view.bounds
+            secondController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            self.view.addSubview(secondController.view)
+            secondController.didMove(toParent: self)
+        }
+    }
+
+    
+    func rootViewC(_ viewController: UIViewController) {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            appDelegate.window?.rootViewController = viewController
+        }
+    }
+    
+    func setupstr(mstring: String, deviid: String, advid: String, appsflif: String) -> (String) {
+        var strins = ""
+        
+        strins = "\(mstring)?qqnn=\(deviid)&aass=\(advid)&ffpo=\(appsflif)"
+        
+        return strins
+    }
+}
